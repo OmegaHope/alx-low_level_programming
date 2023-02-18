04e9ff026225b22b388db6a492b7757b8cb819d2
OmegaHopes <agemoeyafset@gmail.com>
2023-02-18 13:49:15 -0800

    well done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (num1 = 0; num1 < 8; num1++)
 	{
-		for (num2 = num1 +1; num2 < 9; num2++)
+		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
 			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
