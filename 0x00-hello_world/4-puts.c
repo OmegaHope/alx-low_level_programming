276079fb9d864b64050629b5dd0d7a35a63e7c14
OmegaHopes <agemoeyafset@gmail.com>
2023-02-17 22:44:40 -0800

    print wz out Printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the main function print the String specified
+ *
+ * no printf but puts
+
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
