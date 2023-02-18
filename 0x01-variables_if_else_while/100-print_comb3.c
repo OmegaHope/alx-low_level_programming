42975a56dd029ffd490bf3c9ab152f8c6349a705
OmegaHopes <agemoeyafset@gmail.com>
2023-02-18 13:16:21 -0800

    well done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Description: Numbers must be separated by ,, followed by a space
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ *
+ * Return: always return zero(0).
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <=9; num2++)
+		{
+			putchar((num1%10) + '0');
+			putchar((num2%10) + '0');
+			if (num1 == 8)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
