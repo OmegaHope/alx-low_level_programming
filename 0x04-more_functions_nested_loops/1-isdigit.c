967f5a4fd5683fac54aa804311a419272c507088
OmegaHopes <agemoeyafset@gmail.com>
2023-02-24 11:10:59 -0800

    beety fixed

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,15 @@
 /**
  * main - checks for integer
  * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked and return 1 if it is digit
  * Description: int to be checked and return 1 if it is digit
  * Return: otherwise return 0;
  */
 
-int _isdigit(int c) {
-  if (c >= '0' && c <= '9')
-    return (1);
-  else
-    return (0);
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
