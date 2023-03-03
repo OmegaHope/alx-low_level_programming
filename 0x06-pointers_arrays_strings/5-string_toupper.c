8f4cdf104796a1720643742d4a223a2a1722efda
OmegaHopes <agemoeyafset@gmail.com>
2023-03-03 01:11:37 -0800

    done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,20 @@
 #include "main.h"
-
-
-char *string_toupper(char *up)
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
 {
-	char = toUpper;
+	int i;
 
-	toUpper = toupper(up);
-
-	return (toUpper);
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
 }
