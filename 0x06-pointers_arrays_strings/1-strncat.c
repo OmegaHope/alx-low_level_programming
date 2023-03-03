0b3ee0a994029ca423cdff9d88352f6e1bb5b65e
OmegaHopes <agemoeyafset@gmail.com>
2023-03-03 06:30:13 -0800

    fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strncat - concatenate two strings one the after another
+ * _strncat - concatenate two strings one the after another
  * @dest: a pointer variable for the destination string
  * @src: a pointer variable for the source string
  * @n: an integer variable for the number of characters to be append
@@ -11,5 +11,5 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	strncat(dest, src, n);
-	return(dest);
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * strncpy - copy a string from the source to destination
- * @*dest: a pointer variable for the destination string
- * @*src: apointer variable for the source string
+ * _strncpy - copy a string from the source to destination
+ * @dest: a pointer variable for the destination string
+ * @src: apointer variable for the source string
  * @n: an integer variable for the number or size of string to be copied
  * Return: return destination
  */
@@ -10,5 +10,5 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
-	return(dest);
+	return (dest);
 }
