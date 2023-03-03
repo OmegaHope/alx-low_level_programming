9f68a0c654edaf79ffe1d5e9e754adf99ac2282b
OmegaHopes <agemoeyafset@gmail.com>
2023-03-03 05:25:19 -0800

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-/*
+/**
+ * strcat - append or concatenate two strings
+ * @dest: a pointer variable for the destination string
+ * @src: a pointer variable for the source string
  *
- *
- *
- *
+ * Return: return appended string 
  */
 
 char *_strcat(char *dest, char *src)
